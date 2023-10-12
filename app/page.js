8005ef72d6ca2af0ebfde30363bde3233ae5c5cb
blas <blashernandez98@gmail.com>
2023-10-12 13:49:52 -0300

    Updated re-direct path

--- app/page.js
+++ app/page.js
@@ -1,7 +1,9 @@
 import { permanentRedirect } from 'next/navigation'
 
 export default function Home() {
-  permanentRedirect('https://www.google.com')
+  permanentRedirect(
+    'https://twinmotion.unrealengine.com/panorama/arI2grvBC47tfMdw?lang=es-ES'
+  )
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       ...
