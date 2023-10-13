5bb356d8c83b5fbd59239fe33f8dc87b10f7f997
blas <blashernandez98@gmail.com>
2023-10-13 10:19:46 -0300

    New link

--- app/page.js
+++ app/page.js
@@ -1,9 +1,9 @@
 import { permanentRedirect } from 'next/navigation'
 
+const link = 'https://twinmotion.unrealengine.com/panorama/bcWj0szGjxm7_zSN'
+
 export default function Home() {
-  permanentRedirect(
-    'https://twinmotion.unrealengine.com/panorama/arI2grvBC47tfMdw?lang=es-ES'
-  )
+  permanentRedirect(link)
   return (
     <main className='flex min-h-screen flex-col items-center justify-between p-24'>
       ...
